9fb1e5bb766a18969b906567195df359ce61afb5
Jonathan M <jo-m@outlook.com>
2026-03-07 15:18:20 +0100

    make colors themeable

--- private_dot_config/private_fish/conf.d/tide_config.fish
+++ private_dot_config/private_fish/conf.d/tide_config.fish
@@ -5,6 +5,20 @@
 # [Powerline-patched font](https://gist.github.com/1595572).
 
 #
+# Theme colors
+# https://fishshell.com/docs/current/cmds/set_color.html
+#
+
+# 16 colors
+set --local _prompt_colors_black     black
+set --local _prompt_colors_blue      blue
+set --local _prompt_colors_brblack   brblack
+set --local _prompt_colors_green     green
+set --local _prompt_colors_red       red
+set --local _prompt_colors_white     white
+set --local _prompt_colors_yellow    yellow
+
+#
 # Custom segment items
 #
 
@@ -92,17 +106,17 @@ end
 #
 
 # We do not use this directly in a prompt, but it is used by the transient prompt.
-set --global tide_character_color                        yellow
-set --global tide_character_color_failure                yellow
+set --global tide_character_color                       $_prompt_colors_yellow
+set --global tide_character_color_failure               $_prompt_colors_yellow
 if [ (id -u) -eq 0 ]
-    set --global tide_character_icon                     "$hostname #"
+    set --global tide_character_icon                    "$hostname #"
 else
-    set --global tide_character_icon                     "$hostname \$"
+    set --global tide_character_icon                    "$hostname \$"
 end
 
 set --global tide_prompt_add_newline_before             false
-set --global tide_prompt_color_frame_and_connection     brblack
-set --global tide_prompt_color_separator_same_color     brblack
+set --global tide_prompt_color_frame_and_connection     $_prompt_colors_brblack
+set --global tide_prompt_color_separator_same_color     $_prompt_colors_brblack
 set --global tide_prompt_pad_items                      false
 set --global tide_prompt_transient_enabled              true
 
@@ -124,74 +138,74 @@ set --global tide_right_prompt_separator_diff_color     ''
 # Status
 #
 
-set --global tide_private_mode_bg_color    black
-set --global tide_private_mode_color       yellow
-set --global tide_private_mode_icon        "🙈🙉🙊"
+set --global tide_private_mode_bg_color     $_prompt_colors_black
+set --global tide_private_mode_color        $_prompt_colors_yellow
+set --global tide_private_mode_icon         "🙈🙉🙊"
 
-set --global tide_superuser_bg_color       black
-set --global tide_superuser_color          yellow
-set --global tide_superuser_icon           '⚡'
+set --global tide_superuser_bg_color        $_prompt_colors_black
+set --global tide_superuser_color           $_prompt_colors_yellow
+set --global tide_superuser_icon            '⚡'
 
-set --global tide_jobs_bg_color            black
-set --global tide_jobs_color               yellow
-set --global tide_jobs_icon                '⚙'
+set --global tide_jobs_bg_color             $_prompt_colors_black
+set --global tide_jobs_color                $_prompt_colors_yellow
+set --global tide_jobs_icon                 '⚙'
 
 #
 # Rest of left prompt
 #
 
-set --global tide_username_bg_color       blue
-set --global tide_username_color          white
-set --global tide_username_icon           ''
+set --global tide_username_bg_color         $_prompt_colors_blue
+set --global tide_username_color            $_prompt_colors_white
+set --global tide_username_icon             ''
 
-set --global tide_hostname_bg_color       yellow
-set --global tide_hostname_color          black
-set --global tide_hostname_icon           ''
+set --global tide_hostname_bg_color         $_prompt_colors_yellow
+set --global tide_hostname_color            $_prompt_colors_black
+set --global tide_hostname_icon             ''
 
 # PWD
-set --global tide_pwd_bg_color               green
-set --global tide_pwd_color_anchors          white
-set --global tide_pwd_color_dirs             white
-set --global tide_pwd_color_truncated_dirs   white
-set --global tide_pwd_icon                   ''
-set --global tide_pwd_icon_home              ''
-set --global tide_pwd_icon_unwritable        '🚫'
-set --global tide_pwd_markers                .git Cargo.toml go.mod package.json
+set --global tide_pwd_bg_color              $_prompt_colors_green
+set --global tide_pwd_color_anchors         $_prompt_colors_white
+set --global tide_pwd_color_dirs            $_prompt_colors_white
+set --global tide_pwd_color_truncated_dirs  $_prompt_colors_white
+set --global tide_pwd_icon                  ''
+set --global tide_pwd_icon_home             ''
+set --global tide_pwd_icon_unwritable       '🚫'
+set --global tide_pwd_markers               .git Cargo.toml go.mod package.json
 
 # Git
-set --global tide_git_bg_color               red
-set --global tide_git_bg_color_unstable      red
-set --global tide_git_bg_color_urgent        red
-set --global tide_git_color                  white
-set --global tide_git_color_branch           white
-set --global tide_git_color_conflicted       white
-set --global tide_git_color_dirty            white
-set --global tide_git_color_operation        white
-set --global tide_git_color_staged           white
-set --global tide_git_color_stash            white
-set --global tide_git_color_untracked        white
-set --global tide_git_color_upstream         white
-set --global tide_git_icon                   ''
-set --global tide_git_truncation_length      100
-set --global tide_git_truncation_strategy    'l' # Left
+set --global tide_git_bg_color              $_prompt_colors_red
+set --global tide_git_bg_color_unstable     $_prompt_colors_red
+set --global tide_git_bg_color_urgent       $_prompt_colors_red
+set --global tide_git_color                 $_prompt_colors_white
+set --global tide_git_color_branch          $_prompt_colors_white
+set --global tide_git_color_conflicted      $_prompt_colors_white
+set --global tide_git_color_dirty           $_prompt_colors_white
+set --global tide_git_color_operation       $_prompt_colors_white
+set --global tide_git_color_staged          $_prompt_colors_white
+set --global tide_git_color_stash           $_prompt_colors_white
+set --global tide_git_color_untracked       $_prompt_colors_white
+set --global tide_git_color_upstream        $_prompt_colors_white
+set --global tide_git_icon                  ''
+set --global tide_git_truncation_length     100
+set --global tide_git_truncation_strategy   'l' # Left
 
 # Directory environment
-set --global tide_direnv_bg_color            white
-set --global tide_direnv_bg_color_denied     white
-set --global tide_direnv_color               black
-set --global tide_direnv_color_denied        red
-set --global tide_direnv_icon                '🐚'
+set --global tide_direnv_bg_color           $_prompt_colors_white
+set --global tide_direnv_bg_color_denied    $_prompt_colors_white
+set --global tide_direnv_color              $_prompt_colors_black
+set --global tide_direnv_color_denied       $_prompt_colors_red
+set --global tide_direnv_icon               '🐚'
 
-set --global tide_rustc_bg_color             white
-set --global tide_rustc_color                black
-set --global tide_rustc_icon                 '🦀'
+set --global tide_rustc_bg_color            $_prompt_colors_white
+set --global tide_rustc_color               $_prompt_colors_black
+set --global tide_rustc_icon                '🦀'
 
-set --global tide_go_bg_color                white
-set --global tide_go_color                   black
-set --global tide_go_icon                    '🐹'
+set --global tide_go_bg_color               $_prompt_colors_white
+set --global tide_go_color                  $_prompt_colors_black
+set --global tide_go_icon                   '🐹'
 
-set --global tide_python_bg_color           white
-set --global tide_python_color              black
+set --global tide_python_bg_color           $_prompt_colors_white
+set --global tide_python_color              $_prompt_colors_black
 set --global tide_python_icon               '🐍'
 
 #
@@ -199,15 +213,15 @@ set --global tide_python_icon               '🐍'
 #
 
 # Status
-set --global tide_status_bg_color           black
-set --global tide_status_bg_color_failure   red
-set --global tide_status_color              green
-set --global tide_status_color_failure      white
+set --global tide_status_bg_color           $_prompt_colors_black
+set --global tide_status_bg_color_failure   $_prompt_colors_red
+set --global tide_status_color              $_prompt_colors_green
+set --global tide_status_color_failure      $_prompt_colors_white
 set --global tide_status_icon               ''
 set --global tide_status_icon_failure       '↵ '
 
 # Command duration
-set --global tide_cmd_duration_bg_color     yellow
-set --global tide_cmd_duration_color        black
+set --global tide_cmd_duration_bg_color     $_prompt_colors_yellow
+set --global tide_cmd_duration_color        $_prompt_colors_black
 set --global tide_cmd_duration_icon         '⏱'
 set --global tide_cmd_duration_threshold    2000 # ms
